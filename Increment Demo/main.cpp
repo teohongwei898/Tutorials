c0fe910c99061dce75d6f8cf242a23ec281ada57
Hong Wei <teohongwei898@gmail.com>
2017-09-27 12:21:12 +0800

    Increment Demo

---
+++ Increment Demo/main.cpp
@@ -0,0 +1,26 @@
+//How Increment Operator works
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+using namespace std;
+
+int main(int nNumberofArgs, char* pszArgs[])
+{
+    //demonstration
+    int n;
+    //prefix
+    n = 1;
+    cout << "The value of n is: " <<n<<endl;
+    cout << "The value of ++n is: "<<++n<<endl;
+    cout<< "The value of n afterwards is: " <<n<<endl;
+    //suffix
+    n = 1;
+    cout << "The value of n is: " <<n<<endl;
+    cout << "The value of n++ is: "<<n++<<endl;
+    cout<< "The value of n afterwards is: " <<n<<endl
+    //terminate
+    cout <<"Press Enter to continue.." <<endl;
+    cin.ignore(10, '\n')
+    cin.get
+    return 0;
+}
